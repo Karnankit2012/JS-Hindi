551de446afb1e1338203e7cd96496f04b4cc3247
Ankit Karn <karnankit2022@gmail.com>
2025-06-17 12:10:39 +0000

    03

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+//"33" => 33
+//"33abc" => NaN
+// true => 1/ false => 0
+let isLoggedIn = 1;
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+console.log(typeof BooleanIsLoggedIn);
+
