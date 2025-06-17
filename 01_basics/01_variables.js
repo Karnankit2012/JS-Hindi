ae632782049e224c6bda33de97b015c0733955a0
Ankit Karn <karnankit2022@gmail.com>
2025-06-17 11:16:56 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 12345
+let accountEmail = "karnakit2022@gmail.com"
+var accountPassword = "2012"
+accountCity = "Birgunj"
+console.log(accountID)
+console.table([accountID, accountEmail, accountPassword, accountCity])
+//prefer not to use "var"
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //we are using node.js, not browser
+/* 
+number => 2 to power 53
+bigint
+string = " "
+boolean => true/false
+null => stsndalone value
+undefined =>
+symbol => unique
+*/
